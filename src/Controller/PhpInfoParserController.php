cc5e5404c9e7e84d817941f68002752ccb67c4fe
Milos Stojanovic <milosh.stoyanovic@gmail.com>
2021-09-02 19:48:32 +0200

    first attempt at soap server and routes (using annotations)

---
+++ src/Controller/PhpInfoParserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\PhpInfoParser;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PhpInfoParserController extends AbstractController
+{
+
+    /**
+     * @route("/phpinfo")
+     */
+    public function index(PhpInfoParser $phpInfo)
+    {
+        $soapServer = new \SoapServer("path/to/phpinfo.wsdl");
+        $soapServer->setObject($phpInfo);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'text/xml; charset=ISO-8859-1');
+
+        ob_start();
+        $soapServer->handle();
+        $response->setContent(ob_get_clean());
+
+        return $response;
+    }
+
+}
